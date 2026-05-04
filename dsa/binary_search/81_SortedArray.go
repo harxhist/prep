63294fdef617c9f5189958d508363f14a3549775
Harsh <harxhist@gmail.com>
2026-05-04 19:31:38 +0530

    4April
    Signed-off-by: Harsh <harxhist@gmail.com>

---
+++ dsa/binary_search/81_SortedArray.go
@@ -0,0 +1,38 @@
+//https://leetcode.com/problems/search-in-rotated-sorted-array-ii/description/
+
+package main
+
+func search(nums []int, target int) bool {
+    l := 0
+    r := len(nums) - 1
+
+    for l <= r {
+        mid := (l + r) / 2
+
+        if nums[mid] == target {
+            return true
+        }
+
+        if nums[l] == nums[mid] && nums[r] == nums[mid] {
+            l++
+            r--
+            continue
+        }
+
+        if nums[l] <= nums[mid] {
+            if nums[l] <= target && target <= nums[mid] {
+                r = mid - 1 
+            } else {
+                l = mid + 1 
+            }
+        } else {
+            if nums[mid] <= target && target <= nums[r] {
+                l = mid + 1 
+            } else {
+                r = mid - 1
+            }
+        }
+    }
+
+    return false
+}
